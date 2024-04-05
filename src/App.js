07303ac0a7b12bfee535d30e2fa0dea0be96f989
rohangupta845432 <rohangupta845432@gmail.com>
2024-04-05 18:15:07 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+// Write your code at relevant places in the code below
+
+import Expenses from "./components/Expenses";
+
+function App() {
+  const expenses = [
+    { id: "1", date: new Date(2023, 7, 15), title: "Insurance", price: 100 },
+    { id: "2", date: new Date(2023, 3, 25), title: "Book", price: 10 },
+    { id: "3", date: new Date(2023, 10, 11), title: "Pen", price: 1 },
+    { id: "4", date: new Date(2023, 1, 14), title: "Laptop", price: 200 },
+  ];
+
+  return (
+    <div>
+      <h1>Let's get Started</h1>
+      <Expenses expenses={expenses}></Expenses>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,18 @@
+// Write your code at relevant places in the code below
+import Card from "./Card.js";
+import ExpenseDate from "./ExpenseDate.js";
+import "./ExpenseItem.css";
+
+function ExpenseItem(props) {
+    return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date}></ExpenseDate>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.price}</div>
+      </div>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
